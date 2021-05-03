ae979461cd9bf1da1df9f0a120a67fa24a7bbfa3
Ashab <ashabuddin54321@gmail.com>
2021-05-03 09:35:14 -0700

    portfolio

--- src/componemts/Home/Home.js
+++ src/componemts/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                     <h4>Hello1! I am</h4>
                     <h1>ASHAB UDDIN</h1>
                     <h4>Junior web developer</h4>
-                   <button className='btn btn-primary'> <a target='blank' href="https://drive.google.com/uc?id=1PiB6CiF2ahzledvgVJ3Q_czfJGBPqMC1&export=download"></a>Get Resume</button>
+                    <a className='mySkill' target='blank' href="https://drive.google.com/uc?id=1PiB6CiF2ahzledvgVJ3Q_czfJGBPqMC1&export=download">Get Resume</a>
                 </div>
                 
             </section>
